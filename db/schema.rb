a4c27d3959e4121a175216c58e03563d422dd7c7
GreenThaMan <tengisluigi@yahoo.com>
2021-03-26 22:33:24 +0800

    html.erb to slim-rails, and a few other changes
    Used gem 'erb2slim' to convert all views to slim format, and fixed all the problems it caused, along with some other small UX changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_193241) do
+ActiveRecord::Schema.define(version: 2021_03_23_120630) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
